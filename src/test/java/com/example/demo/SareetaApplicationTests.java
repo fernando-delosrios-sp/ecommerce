052a5d1b9a4b8a3d8b33b73670d7688bce7001c1
fernando-delosrios-sp <fernando.delosrios@sailpoint.com>
2021-09-12 21:35:40 +0200

    Some progress with tests

--- src/test/java/com/example/demo/SareetaApplicationTests.java
+++ src/test/java/com/example/demo/SareetaApplicationTests.java
@@ -1,44 +1,85 @@
 package com.example.demo;
 
+import java.util.List;
+
+import com.example.demo.controllers.ItemController;
 import com.example.demo.controllers.UserController;
+import com.example.demo.model.persistence.Item;
 import com.example.demo.model.persistence.User;
 import com.example.demo.model.requests.CreateUserRequest;
 
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+// @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class SareetaApplicationTests {
 	@Autowired
 	private UserController userController;
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	private ItemController itemController;
+
+	private String username = "mytestuser";
+	private String password = "mypassword";
+	private Long id = 1L;
+	private String itemName = "Round Widget";
 
 	@Test
+	@Order(1)
 	public void createUser() {
-		String username = "mytestuser";
-		String password = "mypassword";
-		CreateUserRequest createUserRequest = CreateUserRequest(username, password);
 
-		ResponseEntity<User> response = userController.createUser(createUserRequest);
-		Assertions.assertEquals(response.getBody().getUsername(), username);
-	}
-
-	private CreateUserRequest CreateUserRequest(String username, String password) {
 		CreateUserRequest createUserRequest = new CreateUserRequest();
 		createUserRequest.setUsername(username);
 		createUserRequest.setPassword(password);
 		createUserRequest.setConfirmPassword(password);
 
-		return createUserRequest;
+		ResponseEntity<User> response = this.userController.createUser(createUserRequest);
+		Assertions.assertEquals(response.getBody().getUsername(), username);
 	}
 
+	@Test
+	@Order(2)
+	public void findByUserId() {
+		ResponseEntity<User> response = this.userController.findById(id);
+		Assertions.assertEquals(id, response.getBody().getId());
+	}
+
+	@Test
+	@Order(3)
+	public void findByUserName() {
+		ResponseEntity<User> response = this.userController.findByUserName(username);
+		Assertions.assertEquals(username, response.getBody().getUsername());
+	}
+
+	@Test
+	@Order(4)
+	public void getItems() {
+		ResponseEntity<List<Item>> response = itemController.getItems();
+		Assertions.assertTrue(response.getBody().size() > 0);
+	}
+
+	@Test
+	@Order(5)
+	public void getItemById() {
+		ResponseEntity<Item> response = itemController.getItemById(id);
+		Assertions.assertEquals(id, response.getBody().getId());
+	}
+
+	@Test
+	@Order(6)
+	public void getItemsByName() {
+		ResponseEntity<List<Item>> response = itemController.getItemsByName(itemName);
+		Assertions.assertEquals(itemName, response.getBody().get(0).getName());
+	}
 }
