eb092193b1788838a112023b996bd9f845beb149
fernando-delosrios-sp <fernando.delosrios@sailpoint.com>
2021-09-19 11:48:27 +0200

    Fix for war deployment

--- src/main/java/com/example/demo/SareetaApplication.java
+++ src/main/java/com/example/demo/SareetaApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,7 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableJpaRepositories("com.example.demo.model.persistence.repositories")
 @EntityScan("com.example.demo.model.persistence")
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class SareetaApplication {
+public class SareetaApplication extends SpringBootServletInitializer {
 	private static final Logger log = LoggerFactory.getLogger(SareetaApplication.class);
 
 	@Bean
@@ -25,5 +27,10 @@ public class SareetaApplication {
 		log.info("eCommerce app started."); //Does nothing for some reason...
 		SpringApplication.run(SareetaApplication.class, args);
 	}
+
+	@Override
+  	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+      return builder.sources(SareetaApplication.class);
+  }
 
 }
